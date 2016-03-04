ab3f1feffc6ee0df4ca1c83aa6e5e6d934b4c4eb
Meditate <aroundofyou@gmail.com>
2016-03-04 21:38:23 +0100

    fix articles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @articles=Article.all
     @article=Article.last
     @comments=Comment.where(article_id: @article)
     @random_comment=@comments.order("RANDOM()").first
